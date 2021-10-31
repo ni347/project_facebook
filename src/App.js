3bcbb076c2bd52415562217d588e605e902226bc
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-10-31 14:18:57 +0530

    Home Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Signin from "./components/signin/Signin";
 import Signup from "./components/signup/Signup";
 import Home from "./components/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import {auth } from "./firebase";
+
 function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -12,11 +26,13 @@ function App() {
           <Route path="/" exact>
             <Signin />
           </Route>
+
           <Route path="/Signup">
             <Signup />
           </Route>
+
           <Route path="/Home">
-            <Home />
+            <Home  user={user}/>
           </Route>
         </Switch>
       </Router>
